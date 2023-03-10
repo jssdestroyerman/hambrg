73d09107b4763ee562c6772c81ecb1676e11558d
jssdestroyerman <jesuisdestroyerman@proton.me>
2023-03-10 12:01:33 +0100

    better motion

--- src/lib/components/Menu.svelte
+++ src/lib/components/Menu.svelte
@@ -24,7 +24,7 @@
 	class="absolute top-0 left-[-50px] right-[-50px] bottom-0 bg-[#191919] z-10 -translate-y-[100%] rotate-12 styleMenu1 overflow-hidden"
 >
 	<div
-		class="absolute top-[-50px] bottom-0 right-[-100px] left-0 bg-[#cd2d22] flex justify-center items-center -translate-y-20 rotate-12 styleMenu2"
+		class="absolute top-[-50px] bottom-0 right-[-100px] left-0 bg-[#cd2d22] flex justify-center items-center -translate-y-32 rotate-6 styleMenu2"
 	>
 		<div class="w-[calc(91.666667%_-_50px)] md:w-[calc(75%_-_50px)]">
 			<div class="text-gray-100 flex justify-between items-center">
