3f849a4926986b2f43619cd26ddf1b8de4990c81
jssdestroyerman <jesuisdestroyerman@proton.me>
2023-03-10 19:35:46 +0100

    working

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -1,4 +1,4 @@
-<script>
+<script lang="ts">
 	import { gsap } from 'gsap';
 	import { onMount } from 'svelte';
 	import Menu from './Menu.svelte';
@@ -24,30 +24,36 @@
 			y: 0
 		});
 	});
+
+	function resetState(duration: number) {
+		$menuState.second = true; // wich mean its closed
+		setTimeout(() => {
+			$menuState.third = !$menuState.third;
+			$menuState.first = false;
+			$menuState.second = false;
+		}, duration);
+	}
 </script>
 
 <header class="flex justify-between items-center pt-10 z-20 w-11/12 md:w-3/4 mx-auto">
 	<h1 class="font-semibold text-xl">
-		<a href="/"> HAMBRG. </a>
+		<a href="/" on:click={() => resetState(900)}> HAMBRG. </a>
 	</h1>
 	<button
 		disabled={setDisable}
 		class="text-lg overflow-hidden"
 		on:click={() => {
+			// Disable button after click
 			setDisable = true;
 			setTimeout(() => {
 				setDisable = false;
 			}, 1000);
 
+			// Manage menu state
 			if ($menuState.first === false) {
 				$menuState.first = true; // wich mean its open
 			} else if ($menuState.second === false) {
-				$menuState.second = true; // wich mean its closed
-				setTimeout(() => {
-					$menuState.third = !$menuState.third;
-					$menuState.first = false;
-					$menuState.second = false;
-				}, 900);
+				resetState(900);
 			}
 		}}
 		>{#each 'Menu' as letter}
